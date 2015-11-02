1aef2898fe756b0c1c0abc106f1498c45280f209
endaLFC <emonnelly@gmail.com>
2015-11-02 23:19:34 +0000

    Improvements to bullet class

--- TurretProject/Bullet.cpp
+++ TurretProject/Bullet.cpp
@@ -4,9 +4,54 @@
 
 Bullet::Bullet()
 {
+	if (!m_texture.loadFromFile(" "))
+	{
+		// error...
+	}
+	m_sprite.setTexture(m_texture);
+	m_sprite.setOrigin(1, 1);
+	m_pos = sf::Vector2f(0, 0);
+
+	m_speed = 100;
+	m_rotation = 0;
+	m_direction = sf::Vector2f(cos(toRadians(m_rotation)), sin(toRadians(m_rotation)));
+	m_pos = sf::Vector2f(5, 5);
 }
 
 
 Bullet::~Bullet()
 {
+}
+
+void Bullet::Update(float time)
+{
+	Move(time);
+}
+
+void Bullet::Move(float time)
+{
+	m_sprite.setPosition(m_pos);
+	m_sprite.setRotation(m_rotation);
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) )
+	{
+		
+	}
+
+}
+
+void Bullet::Draw(sf::RenderWindow& window)
+{
+	window.draw(m_sprite);
+}
+
+float Bullet::toRadians(float degrees)
+{
+	return (degrees * 3.14) / 180;
+}
+
+void Bullet::Rotation(int dir, float t)
+{
+	m_rotation += m_speed * t * dir;
+	m_direction = sf::Vector2f(cos(toRadians(m_rotation)), sin(toRadians(m_rotation)));
 }
--- TurretProject/Bullet.h
+++ TurretProject/Bullet.h
@@ -1,14 +1,34 @@
 #pragma once
+#include "SFML\Graphics.hpp"
+
 class Bullet
 {
 	public:
 		Bullet();
 		~Bullet();
 
+		void Update(float time);
+		sf::Vector2f GetPosition();
+		bool GetAlive() { return alive; }
+		void Draw(sf::RenderWindow &window);
+		void Shoot();
 
 
 	protected:
+		sf::Texture m_texture;
+		sf::Sprite m_sprite;
+		sf::Vector2f m_pos;
+		sf::Vector2f m_direction;
 
+		sf::FloatRect m_bounds;
 
+		int m_width;
+		int m_height;
+		float m_speed;
+		float m_rotation;
+		bool alive;
+
+		float toRadians(float degrees);
+		void Rotation(int dir, float t);
+		void Move(float time);
 };
-
